7671c7b64636aff2bcdf37db2cf3265a1aff4fe7
John Joseph <joseph.john33@yahoo.com>
2024-09-06 17:00:58 +0100

    lesson one submission

---
+++ index.js
@@ -0,0 +1,142 @@
+//Task one
+/* let name = "Joseph"
+let greeting = "Hi my name is"
+let aka = "but you can call me Jay"
+
+let greetings = greeting + " " + name + " " + aka
+
+console.log(greetings);
+console.log(greetings.toUpperCase());
+console.log(greetings.toLowerCase());
+
+console.log(greetings.length);
+
+console.log(greetings.slice(0, 13));
+console.log(greetings.slice(14, 21));
+console.log(greetings.slice(21, 44)); */
+
+//Task Two
+/* const num1 = 20
+const num2 = 7
+ 
+let sum = num1 + num2
+let subtract = num1 - num2
+let multiply = num1 * num2
+let divide = num1 / num2
+let modulus = num1 % num2
+
+console.log(sum);
+console.log(subtract);
+console.log(multiply);
+console.log(divide);
+console.log(divide.toFixed());
+console.log(modulus);
+
+ */
+//FIXME: Reme to work on task three
+//Task Three
+/* let isTrue = true
+let isFalse = false
+
+console.log(!isTrue);
+console.log(!isFalse);
+ */
+
+//Task Four
+/* const numbers = [2, 4, 6, 7, 8, 13, 15, 20]
+
+console.log(numbers[0]);
+console.log(numbers[5]);
+
+numbers.pop()
+numbers.push(14, 16, 18)
+numbers.shift()
+numbers.unshift(1,2,3)
+console.log(numbers);
+console.log(numbers.at(3)); */
+
+//Task Five
+/* const person = {
+  name: "Ali",
+  age: 20,
+  location: "Abuja",
+};
+
+let description =
+  "This is" +
+  " " +
+  person.name +
+  ", " +
+  "he is" +
+  " " +
+  person.age +
+  " years old" +
+  " " +
+  "and he lives in" +
+  ", " +
+  person.location;
+
+console.log(description.toUpperCase());
+
+person.height = "179cm"
+
+console.log(person);
+
+ */
+
+//Task Six
+/* let num1 = "30";
+let num2 = "20.33";
+let num3 = 105
+
+console.log(num3.toString());
+console.log(parseInt(num1));
+console.log(parseFloat(num2));
+console.log(num2.split(".")); */
+
+//Task Seven
+/* let name  = "jay"
+let num = 24
+let isTrue = true
+
+console.log(typeof(name));
+console.log(typeof(num));
+console.log(typeof (isTrue));
+
+let num1 = 7
+let num2 = "7"
+
+if (num1 == num2) {
+   console.log(true);
+   
+}
+console.log(false);
+
+if (num1 === num2) {
+   console.log(true);
+   
+} */
+
+//Task Eight
+/* let name;
+let grade = null;
+
+console.log(name);
+console.log(typeof grade);
+ */
+
+//Task Nine
+/* const name = 'Johan'
+let age = 45
+
+let greeting = `Hi my name is ${name} and i'm ${age} years old`
+
+console.log(greeting);
+ */
+
+//Task Ten
+/* console.log(console); */
+console.error('404error');
+console.warn("warning enter a valid input");
+
+
